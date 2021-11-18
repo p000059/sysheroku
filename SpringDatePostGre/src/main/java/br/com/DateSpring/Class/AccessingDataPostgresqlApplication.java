3a59545be3ba5656ace2ed3edc839142d6d9d4d0
p000059 <marcelo.acad@gmail.com>
2021-11-18 16:04:02 -0300

    20211018160300

---
+++ SpringDatePostGre/src/main/java/br/com/DateSpring/Class/AccessingDataPostgresqlApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.DateSpring.Class;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author marcelo
+ */
+@SpringBootApplication
+public class AccessingDataPostgresqlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AccessingDataPostgresqlApplication.class, args);
+  }
+
+}
